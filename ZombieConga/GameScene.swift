6b09d1154c2a274a2f1ce704f9fb9c612a9e03c4
Marvin <marvin.zwolsman@gmail.com>
2017-10-13 14:20:33 +0200

    Added the background

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -12,5 +12,10 @@ import GameplayKit
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.anchorPoint = CGPoint.zero
+        background.position = CGPoint.zero
+        background.zPosition = -1
+        addChild(background)
     }
 }
