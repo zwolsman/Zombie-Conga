14011de05eb62463e2eff6b4791d8c32d0c66e7d
Marvin <marvin.zwolsman@gmail.com>
2017-10-16 09:14:43 +0200

    Bare bones GameOverScene

---
+++ ZombieConga/GameOverScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameOverScene.swift
+//  ZombieConga
+//
+//  Created by Marvin on 16/10/2017.
+//  Copyright © 2017 Zwolsman. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene : SKScene {
+    
+}
